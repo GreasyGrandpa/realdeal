3a46ca1d74503ffd93b5b7bb50232ea9c0b5259e
seXy <matthew.louis@stu.spencer.kyschools.us>
2018-09-14 20:52:27 -0400

    2% done

--- resources/main.js
+++ resources/main.js
@@ -3,22 +3,66 @@ $(document).ready(function(){
     $('.master').fadeIn(1000);
   });
   $('.bannerb').mouseenter(function() {
-    $(this).animate({'opacity':'1'}, 2000);
+    $(this).animate({'opacity':'1'}, 1000);
   });
   $('.bannerc').mouseenter(function(){
-    $(this).animate({'opacity':'1'}, 2000);
+    $(this).animate({'opacity':'1'}, 1000);
   });
-  $('.bannerb').mouseenter(function(){
-    $(this).animate({'background-color':'red'}, 1000);
-  });
+    $('.stand').mouseenter(function(){
+      $(this).animate({'opacity':'1'}, 1000);
+      $('.exitbutton').animate({'opacity':'1'}, 1000);
+      $(this).find('span').css('font-weight','bold');
+      $(this).find('span').animate({'fontSize':'30rem'}, 500);
+      $(this).find('span').animate({'fontSize':'1rem'});
+      $(this).find('.aaaa').css('color','red');
+      $(this).find('.b').css('color','green');
+      $(this).find('.c').css('color','pink');
+      $(this).find('.d').css('color','orange');
+      $(this).find('.e').css('color','blue');
+      $(this).find('.f').css('color','yellow');
+      $(this).find('.g').css('color','purple');
+      $(this).find('.h').css('color','white');
+      $(this).find('span').css('text-decoration','underline');
+    });
+    $('.stand').mouseleave(function(){
+      $(this).find('span').css('color','black');
+      $(this).find('span').css('text-decoration','none');
+    });
   $('.linkish').mouseenter(function(){
-    $(this).animate({'font-size':'1.3rem'}, 500);
+    $(this).animate({'fontSize':'1.3rem'}, 500);
   });
   $('.linkish').mouseleave(function(){
     $(this).finish();
-    $(this).animate({'font-size':'1rem'}, 500);
+    $(this).animate({'fontSize':'1rem'}, 500);
   });
-//wierd slidey button thingeys
+  //jesus button animations
+  $('.exitbutton').mouseenter(function(){
+    $(this).children('p').animate({'fontSize':'1.3rem'}, 500);
+  });
+  $('.exitbutton').mouseleave(function(){
+    $(this).finish();
+    $(this).children('p').animate({'fontSize':'1rem'}, 500);
+  });
+  $('.exitbutton').click(function(){
+    $('.stand').fadeOut(500);
+    $('.exitbutton').fadeOut(500);
+  });
+  //iframe toggle button animations
+  $('.buttonw').mouseenter(function(){
+    $(this).animate({'fontSize':'1.1rem'}, 1000);
+  });
+  $('.buttonw').mouseleave(function(){
+    $(this).finish()
+    $(this).animate({'fontSize':'1rem'});
+  });
+  $('.buttonh').mouseenter(function(){
+    $(this).animate({'fontSize':'1.1rem'}, 1000);
+  });
+  $('.buttonh').mouseleave(function(){
+    $(this).finish()
+    $(this).animate({'fontSize':'1rem'});
+  });
+///wierd slidey button thingeys
   $('.aa').mouseenter(function (){
     $(this).children('.fa').finish();
     $(this).finish();
@@ -68,5 +112,58 @@ $(document).ready(function(){
      $('.state-input').val('');
      $('.city-input').val('');
   });
-  //second button
+  //video height shiz niz
+  $('.master').ready(function(){
+    var height = $('video').css('height');
+    $('.master').find('.placeholdera').html('<div style="height:' + height + ';" class="videocontainer"><div class="mainstuff"><h1>/Professional Web Development</h1></div><video autoplay="" muted="" loop=""><source src="https://www.codecademy.com/assets/marketing-pages/intensives/bws/coding-scenes-9a2031e8142b40bdb5d936d5eef33dfa.mp4" type="video/mp4"></video></div>')
+  });
+  //i-frame schenanigans
+  $('.plusheight').click(function(){
+    var a = $('iframe').css('height');
+    var integer = parseInt(a, 10);
+    var scaleUp = 10;
+    var c = integer + 50;
+    var d = c + 'px';
+    var dd = $('iframe').css('width');
+    $('.placeholder').html('<iframe src="./index.html" height="' + d + '" width="' + dd + '"></iframe>');
+  });
+  $('.minusheight').click(function(){
+    var a = $('iframe').css('height');
+    var integer = parseInt(a, 10);
+    var scaleUp = 10;
+    var c = integer - 50;
+    var d = c + 'px';
+    var dd = $('iframe').css('width');
+    $('.placeholder').html('<iframe src="./index.html" height="' + d + '" width="' + dd + '"></iframe>');
+  });
+  $('.pluswidth').click(function(){
+    var aa = $('iframe').css('width');
+    var integer = parseInt(aa, 10);
+    var scaleUp = 10;
+    var cc = integer + 50;
+    var dd = cc + 'px';
+    var d = $('iframe').css('height');
+    $('.placeholder').html('<iframe src="./index.html" height="' + d + '" width="' + dd + '"></iframe>');
+  });
+  $('.minuswidth').click(function(){
+    var aa = $('iframe').css('width');
+    var integer = parseInt(aa, 10);
+    var scaleUp = 10;
+    var cc = integer - 50;
+    var dd = cc + 'px';
+    var d = $('iframe').css('height');
+    $('.placeholder').html('<iframe src="./index.html" height="' + d + '" width="' + dd + '"></iframe>');
+  });
+  $('.buttonh').click(function(){
+    var height = $('.height-input').val();
+    var dd = $('iframe').css('width');
+    $('.placeholder').html('<iframe src="./index.html" height="' + height + '" width="' + dd + '"></iframe>');
+    $('.height-input').val('');
+  });
+  $('.buttonw').click(function(){
+    var width = $('.width-input').val();
+    var d = $('iframe').css('width');
+    $('.placeholder').html('<iframe src="./index.html" height="' + d + '" width="' + width + '"></iframe>');
+    $('.width-input').val('');
+  });
 });
